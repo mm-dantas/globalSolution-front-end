62c18baa12a6bd26ce25a4434e7aba0692a0f485
devsilasz <salves0306@gmail.com>
2024-06-05 15:24:30 -0300

    Adição de alerts em páginas em construção.

---
+++ script.js
@@ -0,0 +1,18 @@
+// alert('ATENÇÃO! Somos abertos a feedbacks honestos dos nossos projetos, somos estudantes de Engenharia de Software, aptos a bons conselhos');
+
+ourStore = document.querySelector(".our-store");
+pages = document.querySelector(".pages");
+blog = document.querySelector(".blog");
+login = document.querySelector(".login")
+
+ourStore.addEventListener('click', () => {
+    alert('Em construção');
+})
+
+pages.addEventListener('click', () => {
+    alert('Em construção');
+})
+
+blog.addEventListener('click', () => {
+    alert('Em construção');
+})
