2e551000e9a74d8ffd91bcc17bec2d3983fff436
devsilasz <salves0306@gmail.com>
2024-06-06 21:35:12 -0300

    Carrossel finalizado

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ ourStore = document.querySelector(".our-store");
 pages = document.querySelector(".pages");
 blog = document.querySelector(".blog");
 login = document.querySelector(".login");
+postImage1 = document.querySelector("#post-image1");
+postImage2 = document.querySelector("#post-image2");
 
 ourStore.addEventListener('click', () => {
     alert('Em construção');
@@ -28,3 +30,23 @@ login.addEventListener('click', () => {
         alert('Usuario ou senha incorretos');
     }
 })
+
+const carouselContainer = document.querySelector('.carousel-container');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let currentIndex = 0;
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % carouselContainer.children.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + carouselContainer.children.length) % carouselContainer.children.length;
+  updateCarousel();
+});
+
+function updateCarousel() {
+  const slideWidth = carouselContainer.children[0].clientWidth;
+  carouselContainer.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
+}
